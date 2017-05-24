a2304d21b7c878c9e0fcfff0721b6a83cc92675a
Robby Kim <robby@thenewfoundry.com>
2017-05-24 16:21:26 -0400

    Initialize basic file ocnnections

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
 require('babel-polyfill');
 
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+ReactDOM.render(
+	<h1>HI</h1>,
+  	document.getElementById('root')
+);
