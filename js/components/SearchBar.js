e42c050411157acc939292c392ae1cba889b5b76
Robby Kim <robby@thenewfoundry.com>
2017-05-25 11:18:37 -0400

    Remove unnecessary div container of form

--- js/components/SearchBar.js
+++ js/components/SearchBar.js
@@ -4,14 +4,12 @@ var ReactDOM = require('react-dom');
 class SearchBar extends React.Component {
 	render() {
 		return (
-			<div>
-				<form>
-					<input className="search-input" type="text" placeholder="Search..."/>
-					<div className="checkbox-container">
-						<input className="checkbox" type="checkbox" checked="checked" /><span>Only show products in stock</span>
-					</div>
-				</form>
-			</div>
+			<form>
+				<input className="search-input" type="text" placeholder="Search..."/>
+				<div className="checkbox-container">
+					<input className="checkbox" type="checkbox" checked="checked" /><span>Only show products in stock</span>
+				</div>
+			</form>
 		);
 	}
 }
