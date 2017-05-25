0967408819b5f7c4ce08672124bf5880ef8a66b0
Robby Kim <robby@thenewfoundry.com>
2017-05-25 11:11:20 -0400

    Create header for product table

--- js/components/ProductTable.js
+++ js/components/ProductTable.js
@@ -4,9 +4,11 @@ var ReactDOM = require('react-dom');
 class ProductTable extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>hI</h1>
-			</div>
+			<article className="product-table">
+				<div>
+					<h1>Name</h1><h1>Price</h1>
+				</div>
+			</article>
 		);
 	}
 }
