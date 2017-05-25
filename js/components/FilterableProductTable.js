e8f36625b667781f06dbcd3d0ee9dad9475c94d4
Robby Kim <robby@thenewfoundry.com>
2017-05-25 10:57:51 -0400

    Link ProductTable component

--- js/components/FilterableProductTable.js
+++ js/components/FilterableProductTable.js
@@ -2,12 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var SearchBar = require('./SearchBar');
+var ProductTable = require('./ProductTable');
 
 class FilterableProductTable extends React.Component {
 	render() {
 		return (
 			<div>
 				<SearchBar />
+				<ProductTable />
 			</div>
 		);
 	}
---
+++ js/components/ProductTable.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class ProductTable extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>hI</h1>
+			</div>
+		);
+	}
+}
+
+module.exports = ProductTable;
