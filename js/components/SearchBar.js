3c43092e18ecc4cf6174ac6ba0800139dd88a4c5
Robby Kim <robby@thenewfoundry.com>
2017-05-25 15:43:45 -0400

    Implement fully working filterable table! WOOT!

--- js/components/SearchBar.js
+++ js/components/SearchBar.js
@@ -2,12 +2,29 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+		this.handleInStockInputChange = this.handleInStockInputChange.bind(this);
+	}
+
+	handleFilterTextInputChange(e) {
+		this.props.onFilterTextInput(e.target.value);
+	}
+
+	handleInStockInputChange(e) {
+		this.props.onInStockInput(e.target.checked);
+	}
+
 	render() {
 		return (
 			<form>
-				<input className="search-input" type="text" placeholder="Search..." value={this.props.filterText}/>
+				<input className="search-input" type="text" placeholder="Search..." value={this.props.filterText}
+					onChange={this.handleFilterTextInputChange} />
 				<div className="checkbox-container">
-					<input className="checkbox" type="checkbox" checked={this.props.inStockOnly} /><span> Only show products in stock</span>
+					<input className="checkbox" type="checkbox" checked={this.props.inStockOnly} 
+						onChange={this.handleInStockInputChange} /><span> Only show products in stock</span>
 				</div>
 			</form>
 		);
